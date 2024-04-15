cda87a3cc50189bdeb496e147fb22e9c348cf627
Dyo2000 <118697890+Dyo2000@users.noreply.github.com>
2024-04-15 19:20:13 -0400

    Create IOrbit.java

---
+++ Project_3/SolarSystem/IOrbit.java
@@ -0,0 +1,10 @@
+package solarsystem;
+/**
+ * This is the IOrbit interface
+ * has the getOrbit method
+ */
+public interface IOrbit {
+    CelestialBody getOrbit();
+    void getOrbitInformation();
+
+}
