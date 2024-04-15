de9b9f66a3ba05f8304ddcc97013d17cf43dad17
Dyo2000 <118697890+Dyo2000@users.noreply.github.com>
2024-04-15 19:17:42 -0400

    Create DuplicateCelestialBodyException.java

---
+++ Project_3/Exceptions/DuplicateCelestialBodyException.java
@@ -0,0 +1,10 @@
+package exceptions;
+/**
+ * This is the DuplicateCelestialBodyException Class that extends Exception
+ * This class displays an argMessage when thrown
+ */
+public class DuplicateCelestialBodyException extends Exception{
+    public DuplicateCelestialBodyException(String argMessage){
+        super(argMessage);
+    }
+}
